6566d5b031b4ba727cb418b6411889ef9994fed4
Piotr Orlowski (orlowskp) <orlowskp@coventry.ac.uk>
2020-09-25 13:02:26 +0200

    initial commit

---
+++ PasswordManagerCLI.py
@@ -0,0 +1,10 @@
+
+
+MASTER_PASSWORD = "1234qwer"
+
+print("#"*35)
+print("# Welcome to Password Manager CLI #")
+print("#"*35)
+
+connect = input("""\nPlease enter your master password to connect
+-->""")
